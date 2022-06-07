9e236b820d3382de57eabd8551e07d6d83509b7b
= <thiago.chinen@plasson.com.br>
2022-06-07 14:03:56 -0300

    Fix: Adjustments in switchlanguage component

--- src/components/LanguageSwitch/index.tsx
+++ src/components/LanguageSwitch/index.tsx
@@ -1,28 +1,27 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { MdLanguage } from "react-icons/md";
 
 const LanguageSwitch = () => {
- const { locales, locale, pathname, query, asPath } = useRouter();
- const otherLocales = locales && locales.filter((l) => l !== locale) || [];
+  const router = useRouter();
+  const { locales, pathname, query, asPath } = router;
+  const otherLocales = locales || [];
 
- return (
-   <>
-    <MdLanguage />
-     {otherLocales.map((locale) => {
-       return (
-         <Link
-           key={locale}
-           href={{ pathname, query }}
-           as={asPath}
-           locale={locale}
-         >
-           <a>Switch to &quot;{locale}&quot;</a>
-         </Link>
-       );
-     })}
-   </>
- );
+  return (
+    <>
+      <section className="flex items-center">
+        <MdLanguage size={40} />
+        <select className="appearance-none text-semibold text-xl p-1 rounded bg-bg-color transition focus:outline-none"
+          onChange={(event) => router.push({ pathname, query }, asPath, { locale: event.target.value })}
+        >
+          {otherLocales.map((localeOption) => {
+            return (
+              <option value={localeOption}>{localeOption.toUpperCase()}</option>
+            );
+          })}
+        </select>
+      </section>
+    </>
+  );
 }
 
 export default LanguageSwitch;
