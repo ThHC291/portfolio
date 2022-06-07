338bb72f4d3c2b0e2d0a74658a7a8b74c6c20232
= <thiago.chinen@plasson.com.br>
2022-06-07 14:18:01 -0300

    Fix: Adjustments in Links component

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -1,11 +1,28 @@
+import Link from 'next/link';
 import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa';
 
 const Links: React.FC = () => {
   return (
     <>
-      <FaGithub />
-      <FaLinkedin />
-      <FaInstagram />
+      <section className="flex items-center">
+        <section className="px-1">
+          <Link href="https://github.com/ThHC291">
+            <a target="_blank"><FaGithub size={40} /></a>
+          </Link>
+        </section>
+
+        <section className="px-1">
+          <Link className="w-full" href="">
+            <a target="_blank"><FaLinkedin size={40} /></a>
+          </Link>
+        </section>
+
+        <section className="px-1">
+          <Link href="https://www.instagram.com/thiago_chinen/">
+            <a target="_blank"><FaInstagram size={40} /></a>
+          </Link>
+        </section>
+      </section>
     </>
   )
 }
