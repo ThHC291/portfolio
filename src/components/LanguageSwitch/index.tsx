317b6963ade514a2eb1e8790e5e96d14646cf4f3
= <thiago.chinen@plasson.com.br>
2022-06-07 14:43:52 -0300

    Fix: Adjustments in switchLanguage component

--- src/components/LanguageSwitch/index.tsx
+++ src/components/LanguageSwitch/index.tsx
@@ -10,12 +10,12 @@ const LanguageSwitch = () => {
     <>
       <section className="flex items-center">
         <MdLanguage size={40} />
-        <select className="appearance-none text-semibold text-xl p-1 rounded bg-bg-color transition focus:outline-none"
+        <select className="text-semibold text-xl p-1 rounded bg-transparent	transition focus:outline-none"
           onChange={(event) => router.push({ pathname, query }, asPath, { locale: event.target.value })}
         >
           {otherLocales.map((localeOption) => {
             return (
-              <option value={localeOption}>{localeOption.toUpperCase()}</option>
+              <option className="dark:bg-bg-color" value={localeOption}>{localeOption.toUpperCase()}</option>
             );
           })}
         </select>
