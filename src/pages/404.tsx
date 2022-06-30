61ddcc85580fb166a461b4abfabfb5f2f0a71e45
= <thiago.chinen@plasson.com.br>
2022-06-30 10:48:15 -0300

    Feature: finished 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,30 +1,33 @@
 // 404.js
 import { NextPage } from 'next';
+import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 
 const FourOhFour: NextPage = () => {
-	return (
-		<>
-			<div className="container flex flex-wrap h-screen overflow-y-auto p-4 mx-auto md:pl-24 lg:pl-24 xl:pl-24">
+  const t = useTranslations('pages.404');
 
-        <div className="flex flex-grow flex-col md:w-2/5 justify-center items-start text-center md:text-left lg:w-1/2 xl:w-1/2">
-          <div className="w-full my-3 text-4xl font-bold text-primary lg:text-9xl xl:text-8xl">
-						404
-          </div>
-          <div className="w-full text-xl font-semibold lg:text-4xl xl:text-4xl">
-						Page not found!<br/>
-						Be careful there could be ghosts here.
-          </div>
-        </div>
+  return (
+    <>
+      <section className="container flex flex-col items-center p-4 xl:flex-row gap-x-12">
 
-        <div className="flex flex-wrap py-6">
-					<Image src="/ghost.png" width={550} height={400}></Image>
-        </div>
+        <section className="flex flex-grow flex-col md:w-2/5 justify-center items-start text-center md:text-left lg:w-1/2 xl:w-1/2">
+          <section className="w-full my-3 text-9xl font-bold text-primary">
+            404
+          </section>
+          <section className="w-full text-xl font-semibold lg:text-4xl xl:text-4xl">
+            {t('errorMessage')}
+            <br />
+            {t('funnyMessage')}
+          </section>
+        </section>
 
-      </div>
+        <section className="my-6">
+          <Image src="/ghost.png" width={700} height={600}></Image>
+        </section>
 
-		</>
-	);
+      </section>
+    </>
+  );
 };
 
 export default FourOhFour;
