5c81a334f2fc569e91939aa6961f110102996623
= <thiago.chinen@plasson.com.br>
2022-07-01 10:44:32 -0300

    Feature: Initial adjustments in projects page

---
+++ src/components/CardProject/index.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FaGithub, FaLink, FaListUl } from "react-icons/fa";
+import { LinkProject } from "../../pages/projects";
+
+interface CardProps {
+    image: string;
+    title: string;
+    description: string;
+    techs: string;
+    links: LinkProject;
+}
+
+const CardProject: React.FC<CardProps> = ({ image, title, description, techs, links }: CardProps) => {
+    return (
+        <>
+            <div className="flex flex-col justify-between rounded bg-secondary overflow-hidden shadow-lg transform  hover:scale-105 transition duration-300">
+
+                <Image className="w-full" layout="responsive" width="100" height="70" src={image} alt="Project photo" />
+
+                <div className="flex flex-col items-center">
+                    <div className="font-bold text-2xl p-2">{title}</div>
+                    <div className="w-36 border-t border-primary"></div>
+                    <p className="max-w-sm text-center text-sm p-4">
+                        {description}
+                    </p>
+                    <div className="w-36 border-t border-primary"></div>
+                    <div className="p-2">
+                        {techs}
+                    </div>
+                </div>
+
+                <div className="flex flex- justify-between space-x-4 nowrap text-xs font-semibold bg-main-color p-2 md:text-sm">
+                    <Link href={links.detail}>
+                        <a className="flex justify-center items-center space-x-1 hover:text-primary">
+                            <FaListUl />
+                            <p>View detail</p>
+                        </a>
+                    </Link>
+                    <Link href={links.preview}>
+                        <a className="flex justify-center items-center space-x-1 hover:text-primary">
+                            <FaLink />
+                            <p>Link preview</p>
+                        </a>
+                    </Link>
+                    <Link href={links.detail}>
+                        <a className="flex justify-center items-center space-x-1 hover:text-primary">
+                            <FaGithub />
+                            <p>Repository</p>
+                        </a>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default CardProject;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,8 +1,46 @@
 import { NextPage } from 'next'
+import CardProject from '../components/CardProject';
+
+export interface LinkProject {
+  detail: string;
+  preview: string;
+  repository: string;
+}
+interface Project {
+  image: string;
+  title: string;
+  description: string;
+  techs: string;
+  links: LinkProject;
+}
+
+const projects: Array<Project> = [
+  { image: '/photo.jfif', title: 'Project title', description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum has been the industry's standard dummy text ever since the 1500s`, techs: 'NextJS', links: { detail: '/home', preview: '/home', repository: '/home' } },
+  { image: '/photo.jfif', title: 'Project title', description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem standard dummy text ever since the 1500s`, techs: 'NextJS', links: { detail: '/home', preview: '/home', repository: '/home' } },
+  { image: '/photo.jfif', title: 'Project title', description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum has been the industry's standard dummy text ever since the 1500s`, techs: 'NextJS', links: { detail: '/home', preview: '/home', repository: '/home' } },
+  { image: '/photo.jfif', title: 'Project title', description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum has been the industry's standard dummy text ever since the 1500s`, techs: 'NextJS', links: { detail: '/home', preview: '/home', repository: '/home' } },
+  { image: '/photo.jfif', title: 'Project title', description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.Lorem Ipsum has been the industry's standard dummy text ever since the 1500sLorem Ipsum has been the industry's standard dummy text ever since the 1500s`, techs: 'NextJS', links: { detail: '/home', preview: '/home', repository: '/home' } },
+  { image: '/photo.jfif', title: 'Project title', description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem`, techs: 'NextJS', links: { detail: '/home', preview: '/home', repository: '/home' } },
+];
 
 const Projects: NextPage = () => {
   return (
-    <div>Projects</div>
+    <>
+      <div className="container flex flex-col items-center p-4 xl:flex-row gap-x-12 xl:mb-12">
+        <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-10">
+          {projects.map((project, index) => (
+            <CardProject
+              key={index}
+              image={project.image}
+              title={project.title}
+              description={project.description}
+              techs={project.techs}
+              links={project.links}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 
