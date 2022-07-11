eecb454584c4cc10adfc44c547306dc604f7a409
Thiago Chinen <thiagochinen2015@gmail.com>
2022-07-11 22:28:12 +0000

    Fix: Adjustments in home and 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,30 +4,28 @@ import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 
 const FourOhFour: NextPage = () => {
-  const t = useTranslations('pages.404');
+	const t = useTranslations('pages.404');
 
-  return (
-    <>
-      <section className="container flex flex-col items-center p-4 xl:flex-row gap-x-12">
+	return (
+		<>
+			<section className="container flex flex-col items-center p-4 xl:flex-row gap-x-12">
+				<section className="flex flex-grow flex-col md:w-2/5 justify-center items-start text-center md:text-left lg:w-1/2 xl:w-1/2">
+					<section className="w-full my-3 text-9xl font-bold text-primary">
+						404
+					</section>
+					<section className="w-full text-xl font-semibold lg:text-3xl">
+						{t('errorMessage')}
+						<br />
+						{t('funnyMessage')}
+					</section>
+				</section>
 
-        <section className="flex flex-grow flex-col md:w-2/5 justify-center items-start text-center md:text-left lg:w-1/2 xl:w-1/2">
-          <section className="w-full my-3 text-9xl font-bold text-primary">
-            404
-          </section>
-          <section className="w-full text-xl font-semibold lg:text-4xl xl:text-4xl">
-            {t('errorMessage')}
-            <br />
-            {t('funnyMessage')}
-          </section>
-        </section>
-
-        <section className="my-6">
-          <Image src="/ghost.png" width={700} height={600}></Image>
-        </section>
-
-      </section>
-    </>
-  );
+				<section className="flex items-center justify-center rounded-xl py-6">
+					<Image src="/ghost.png" width={700} height={600}></Image>
+				</section>
+			</section>
+		</>
+	);
 };
 
 export default FourOhFour;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,31 +3,36 @@ import { useTranslations } from 'next-intl';
 import Image from 'next/image';
 
 const Home: NextPage = () => {
-  const t = useTranslations('Home');
+	const t = useTranslations('Home');
 
-  return (
-    <>
-      <div className="container flex flex-col items-center p-4 md:pl-24 lg:pl-24 xl:pl-24 xl:flex-row gap-x-12 xl:mb-12">
+	return (
+		<>
+			<section className="container flex flex-col items-center p-4 md:flex-row gap-x-12 xl:mb-12">
+				<section className="flex flex-grow flex-col md:w-2/5 justify-center items-start text-center md:text-left lg:w-1/2 xl:w-1/2">
+					<section className="w-full font-semibold text-xl lg:text-4xl xl:text-4xl">
+						{t('helloMessage')}
+					</section>
+					<section className="w-full my-3 text-4xl font-bold text-primary lg:text-8xl xl:text-8xl">
+						Thiago Chinen
+					</section>
+					<section className="w-full text-xl font-semibold lg:text-4xl xl:text-4xl">
+						{t('desecription')}
+						<br />
+						{t('slogan')}
+					</section>
+				</section>
 
-        <div className="flex flex-grow flex-col md:w-2/5 justify-center items-start text-center md:text-left lg:w-1/2 xl:w-1/2">
-          <div className="w-full font-semibold text-xl lg:text-4xl xl:text-4xl">
-            { t('helloMessage') }
-          </div>
-          <div className="w-full my-3 text-4xl font-bold text-primary lg:text-8xl xl:text-8xl">
-            Thiago Chinen
-          </div>
-          <div className="w-full text-xl font-semibold lg:text-4xl xl:text-4xl">
-            Software developer, living in Brasil.<br></br>
-            Always trying learning something new.
-          </div>
-        </div>
-
-        <div className="flex items-center justify-center bg-main-color py-6">
-          <Image src="/photo.jfif" layout="intrinsic" width={400} height={300}/>
-        </div>
-      </div>
-    </>
-  );
-}
+				<section className="flex items-center bg-main-color justify-center rounded-xl py-6">
+					<Image
+						src="/photo.jfif"
+						layout="intrinsic"
+						width={400}
+						height={300}
+					/>
+				</section>
+			</section>
+		</>
+	);
+};
 
 export default Home;
