5074e94055475e2c52d6250c50dc989fde8e4bbe
Thiago Chinen <thiagochinen2015@gmail.com>
2022-07-11 22:28:42 +0000

    Fix: Adjustment in size of pages

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,11 +8,11 @@ interface LayoutProps {
 const Layout: React.FC<LayoutProps> = ({ children }) => {
 	return (
 		<>
-			<div className="flex flex-col justify-between h-screen">
+			<section className="flex flex-col justify-between min-h-screen">
 				<NavBar />
 				<section className="mx-auto">{children}</section>
 				<Footer />
-			</div>
+			</section>
 		</>
 	);
 };
